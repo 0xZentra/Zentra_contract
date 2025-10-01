dfd9b77d69722e60e503b170a04a6842473043c0
KJ <kernel1983@gmail.com>
2025-10-01 19:36:00 -0400

    withdraw yield

--- airdrop.sol
+++ airdrop.sol
@@ -1,15 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-
 interface IERC20 {
-    // event Transfer(address indexed from, address indexed to, uint256 value);
-    // event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    // function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 value) external returns (bool);
-    // function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 value) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
@@ -17,6 +11,7 @@ interface IERC20 {
 interface IAave {
     function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
     function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+    function getReserveAToken(address asset) external view returns (address);
 }
 
 contract Airdrop {
@@ -29,19 +24,13 @@ contract Airdrop {
     uint256 public airdropEndtime;
     bool public depositEnabled;
     address aaveProxy; // 0xA238Dd80C259a72e81d7e4664a9801593F98d1c5
-    // uint256 public minDepositAmount;
-    // uint256 public totalReward;
 
     struct Deposit {
         address token;
         uint256 amount;
-        // uint256 lockTime;
-        // address referral;
     }
     mapping(address => Deposit) public deposits;
-    // mapping(address => uint256) public rewards;
 
-    event TokenTypeAdded(address indexed token);
     event AirdropEndChanged(uint256 timestamp);
     event AirdropDepositChanged(address indexed user, uint256 amount, uint256 timestamp);
 
@@ -60,22 +49,18 @@ contract Airdrop {
         operator = msg.sender;
         depositEnabled = true;
         aaveProxy = _aaveProxy;
-        // minDepositAmount = 20 * 10**6;
-        // totalReward = 0;
     }
 
-    function addToken(address _tokenAddress) public onlyOperator {
+    function addToken(address _tokenAddress) public onlyOwner {
         require(_tokenAddress != address(0), "Invalid token address");
         require(!supportedTokens[_tokenAddress], "Token already supported");
         supportedTokens[_tokenAddress] = true;
         supportedTokenList.push(_tokenAddress);
-        emit TokenTypeAdded(_tokenAddress);
     }
 
     function deposit(address _token, uint256 _amount) public {
         require(depositEnabled, "Deposits are disabled");
         require(supportedTokens[_token], "Token not supported");
-        // require(_amount >= minDepositAmount, "Amount not enough");
 
         IERC20 token = IERC20(_token);
         token.transferFrom(msg.sender, address(this), _amount);
@@ -84,76 +69,25 @@ contract Airdrop {
         deposits[msg.sender] = Deposit({
             token: _token,
             amount: _amount
-            // lockTime: block.timestamp,
-            // referral: _referral
         });
-        aaveDeposit(_token, _amount);
+
+        IERC20(_token).approve(aaveProxy, _amount);
+        IAave(aaveProxy).supply(_token, _amount, address(this), 0);
     }
 
     function withdraw(uint256 _amount) public {
-        // require(_depositIndex < deposits[msg.sender].length, "Invalid deposit index");
-
         Deposit storage userDeposit = deposits[msg.sender];
-        // require(block.timestamp >= userDeposit.lockTime + 3600, "Lock period not over yet");
         userDeposit.amount -= _amount;
         address tokenAddress = userDeposit.token;
-        aaveWithdraw(tokenAddress, _amount);
-        // uint256 locktime = userDeposit.lockTime;
-        // address referral = userDeposit.referral;
 
         deposits[msg.sender] = userDeposit;
-        // deposits[msg.sender].pop();
         totalDepositedByToken[tokenAddress] -= _amount;
+        IAave(aaveProxy).withdraw(tokenAddress, _amount, address(this));
 
         IERC20 token = IERC20(tokenAddress);
         token.transfer(msg.sender, _amount);
 
-        // uint256 duration;
-	    // if (airdropEndtime == 0 || airdropEndtime > block.timestamp){
-	    //     duration = block.timestamp - locktime;
-	    // }else if (airdropEndtime <= block.timestamp){
-	    //     duration = airdropEndtime - locktime;
-	    // }
-        // uint256 rewarded_tokens = 10**18 * (amount * duration) / (1000*10**6 * 365 days);
-        // rewards[msg.sender] += rewarded_tokens;
-        // totalReward += rewarded_tokens;
         emit AirdropDepositChanged(msg.sender, userDeposit.amount, block.timestamp);
-    }
-
-    // function getReward(address _user) public view returns (uint256) {
-    //     uint256 reward = rewards[_user];
-    //     for (uint d = 0; d < deposits[_user].length; d ++) {
-    //         Deposit memory depositToWithdraw = deposits[_user][d];
-    //         uint256 amount = depositToWithdraw.amount;
-    //         uint256 locktime = depositToWithdraw.lockTime;
-    //         uint256 duration;
-	//         if (airdropEndtime == 0 || airdropEndtime > block.timestamp){
-	//             duration = block.timestamp - locktime;
-	//         }else if (airdropEndtime <= block.timestamp){
-	//             duration = airdropEndtime - locktime;
-	//         }
-
-    //         uint256 rewarded_tokens = 10**18 * (amount * duration) / ( 1000*10**6 * 365 days);
-    //         reward += rewarded_tokens;
-    //     }
-
-    //     return reward;
-    // }
-
-    // function getDepositsLength(address _user) public view returns (uint256) {
-    //     return deposits[_user].length;
-    // }
-
-
-    function aaveDeposit(address _asset, uint256 _amount) internal {
-        // require(msg.sender == operator, OPERATOR_REQUIRED_ERROR);
-        IERC20(_asset).approve(aaveProxy, _amount);
-        IAave(aaveProxy).supply(_asset, _amount, address(this), 0);
-    }
-
-    function aaveWithdraw(address _asset, uint256 _amount) internal {
-        // require(msg.sender == operator, OPERATOR_REQUIRED_ERROR);
-        IAave(aaveProxy).withdraw(_asset, _amount, address(this));
     }
 
 
@@ -176,17 +110,13 @@ contract Airdrop {
         emit AirdropEndChanged(_timestamp);
     }
 
-    // function setMinDepositAmount(uint256 _newAmount) public onlyOperator {
-    //     minDepositAmount = _newAmount;
-    // }
-
     function checkYieldToken(address _tokenAddress) public view returns (uint256) {
-        IERC20 token = IERC20(_tokenAddress);
-        uint256 contractBalance = token.balanceOf(address(this));
-        uint256 depositedAmount = totalDepositedByToken[_tokenAddress];
+        IERC20 aToken = IERC20(IAave(aaveProxy).getReserveAToken(_tokenAddress));
+        uint256 aTokenBalance = aToken.balanceOf(address(this));
+        uint256 depositedTotal = totalDepositedByToken[_tokenAddress];
 
-        if (contractBalance > depositedAmount) {
-            return contractBalance - depositedAmount;
+        if (aTokenBalance > depositedTotal) {
+            return aTokenBalance - depositedTotal;
         } else {
             return 0;
         }
@@ -195,15 +125,12 @@ contract Airdrop {
     function withdrawYieldTokens() public onlyOperator {
         for (uint i = 0; i < supportedTokenList.length; i++) {
             address tokenAddress = supportedTokenList[i];
-            IERC20 token = IERC20(tokenAddress);
-            uint256 contractBalance = token.balanceOf(address(this));
-            uint256 depositedAmount = totalDepositedByToken[tokenAddress];
+            IERC20 aToken = IERC20(IAave(aaveProxy).getReserveAToken(tokenAddress));
+            uint256 yieldAmount = checkYieldToken(tokenAddress);
 
-            if (contractBalance > depositedAmount) {
-                uint256 excessAmount = contractBalance - depositedAmount;
-                token.transfer(owner, excessAmount);
+            if (yieldAmount > 0) {
+                aToken.transfer(owner, yieldAmount);
             }
         }
     }
 }
-
